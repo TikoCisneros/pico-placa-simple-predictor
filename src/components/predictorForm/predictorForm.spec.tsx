0a16ad7c39189c1ffcb87518ef86c0669c9be37f
LACG <lacisner@pichincha.com>
2021-06-12 23:31:18 -0500

    fix PredictorForm test

--- src/components/predictorForm/predictorForm.spec.tsx
+++ src/components/predictorForm/predictorForm.spec.tsx
@@ -5,12 +5,15 @@ import PickersProvider from '../providers/pickersProvider';
 
 describe('PredictorForm test', () => {
   it('should renders correctly', () => {
+    const userDate = '2020-06-12T13:17:41.707Z';
+
     const props = {
       licensePlate: '',
       onLicensePlateChange: jest.fn(),
-      dateTime: new Date(),
+      dateTime: new Date(userDate),
       onDateTimeSelect: jest.fn(),
     };
+
     const { asFragment } = render(
       <PickersProvider>
         <PredictorForm {...props} />
